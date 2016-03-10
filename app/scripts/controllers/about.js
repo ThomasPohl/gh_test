d8e6bf723e3cba0f3f7bcf77e11e1738357f8037
thpo <thomas.pohl@exxeta.de>
2016-03-10 13:36:06 +0100

    Initial checkin

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ghTestJoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ghTestJoApp
+ */
+angular.module('ghTestJoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
